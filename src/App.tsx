44b70cfc0fd59cde79bc003b2c81c024d9a10a64
Justice Dunn <justicedunn@me.com>
2021-03-01 13:46:23 -0700

    Create blog page skeleton, change Home.jsx to Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./scenes/homePage/Home.jsx";
+import Home from "./scenes/homePage/Home";
+import Blog from "./scenes/blogPage/Blog";
 import NavBar from './components/navbar/navbar';
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route path="/" component={Home} exact>
             <Home />
+          </Route>
+          <Route path="/blog" component={Blog}>
+            <Blog />
           </Route>
         </Switch>
       </div>
---
+++ src/scenes/blogPage/Blog.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import "./blog.css";
+
+export default class Home extends Component {
+    render() {
+        return <h1>Hello, Blog!</h1>
+    }
+}
