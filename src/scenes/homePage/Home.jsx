05424f9868d8b989de4fa1ff1db94de537dd3dc7
Justice Dunn <justicedunn@me.com>
2021-03-01 12:49:11 -0700

    Complete about section of home page

--- src/scenes/homePage/Home.jsx
+++ src/scenes/homePage/Home.jsx
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
+
+import "./home.css";
 
 export default class Home extends Component {
   render() {
-    return <h1>Hello, home!</h1>;
+    return (
+      <Container>
+        <div className="about">
+          <Row>
+            <p>
+              I'm a <span>Full-Stack Web Engineer</span> with a knack for
+              problem-solving. I consider myself a jack-of-all-trades with
+              specific interest and experience in:
+            </p>
+            <ul>
+              <li>Backend Development</li>
+              <li>Dart/Flutter</li>
+              <li>React-Bootstrap</li>
+            </ul>
+          </Row>
+        </div>
+        <div className="portfolio">
+          <Row>
+            <p>Portfolio</p>
+          </Row>
+        </div>
+        <div className="contact">
+          <Row>
+            <p>Contact</p>
+          </Row>
+        </div>
+      </Container>
+    );
   }
 }
