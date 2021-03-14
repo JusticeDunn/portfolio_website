d4cae2a276e79729966dddbc04666c65e387aae4
Justice Dunn <justicedunn@me.com>
2021-03-14 16:57:16 -0600

    Begin restyling homepage

--- src/scenes/homePage/Home.tsx
+++ src/scenes/homePage/Home.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 import "./home.css";
 
@@ -8,43 +8,50 @@ export default class Home extends Component {
   render() {
     return (
       <Container>
+        <Col>
         <div className="about">
           <Row>
             <p>
-              I'm a <span>Full-Stack Web Engineer</span> with a knack for
-              problem-solving. I consider myself a jack-of-all-trades with
-              specific interest and experience in:
+              I'm <span>Justice</span>, and I'm a <span>Full-Stack Web Engineer</span>.
             </p>
-            <ul className="bold">
-              <li>Backend Development</li>
-              <li>Dart/Flutter</li>
-              <li>React-Bootstrap</li>
-            </ul>
+            <p>
+              I make mobile and web apps using everything ranging from <span>JavaScript</span> (ES6+)
+              and the <span>MERN</span> stack to <span>Dart</span> and <span>Flutter</span>.
+            </p>
           </Row>
         </div>
         <hr />
         <div className="portfolio">
           <Row>
             <p>Here are some of the projects I've worked on:</p>
-            <ul>
-              <li>
-                <span>He'inon</span> - An Arapaho language learning app
-              </li>
-            </ul>
-            <p>Visit my GitHub to look into my inolvement in other projects!</p>
+          </Row>
+          <Row>
+            <Card style={{ width: '30%', backgroundColor: 'gray', color:'white' }}>
+              <Card.Body>
+                <Card.Title><p>He'inon</p></Card.Title>
+                <Card.Text>
+                  An Arapaho language learning app.
+                </Card.Text>
+                <Button variant="primary" className="text">More Info</Button>
+              </Card.Body>
+            </Card>
           </Row>
         </div>
-        <hr />
         <div className="contact">
           <Row id="contact">
             <p>
-              Contact me via LinkedIn or at{" "}
+              For more info{" "}
               <a href="mailto:realjusticedunn@gmail.com">
-                realjusticedunn@gmail.com
+                <span>contact me</span>
               </a>
+              {" "}or check me out in the links below!
             </p>
           </Row>
         </div>
+        </Col>
+        <Col>
+          <img src={require("../../images/me.jpg")} alt="me"/>
+        </Col>
       </Container>
     );
   }
