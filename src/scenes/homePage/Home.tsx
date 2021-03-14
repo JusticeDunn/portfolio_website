d0f38c11ab4f3349848b04d7fb5a159f7fffb822
Justice Dunn <justicedunn@me.com>
2021-03-14 17:16:04 -0600

    Make home photo disappear when window is small

--- src/scenes/homePage/Home.tsx
+++ src/scenes/homePage/Home.tsx
@@ -8,50 +8,52 @@ export default class Home extends Component {
   render() {
     return (
       <Container>
-        <Col>
-        <div className="about">
-          <Row>
-            <p>
-              I'm <span>Justice</span>, and I'm a <span>Full-Stack Web Engineer</span>.
-            </p>
-            <p>
-              I make mobile and web apps using everything ranging from <span>JavaScript</span> (ES6+)
-              and the <span>MERN</span> stack to <span>Dart</span> and <span>Flutter</span>.
-            </p>
-          </Row>
-        </div>
-        <hr />
-        <div className="portfolio">
-          <Row>
-            <p>Here are some of the projects I've worked on:</p>
-          </Row>
-          <Row>
-            <Card style={{ width: '30%', backgroundColor: 'gray', color:'white' }}>
-              <Card.Body>
-                <Card.Title><p>He'inon</p></Card.Title>
-                <Card.Text>
-                  An Arapaho language learning app.
-                </Card.Text>
-                <Button variant="primary" className="text">More Info</Button>
-              </Card.Body>
-            </Card>
-          </Row>
-        </div>
-        <div className="contact">
-          <Row id="contact">
-            <p>
-              For more info{" "}
-              <a href="mailto:realjusticedunn@gmail.com">
-                <span>contact me</span>
-              </a>
-              {" "}or check me out in the links below!
-            </p>
-          </Row>
-        </div>
-        </Col>
-        <Col>
-          <img src={require("../../images/me.jpg")} alt="me"/>
-        </Col>
+        <Row>
+          <Col>
+          <div className="about">
+            <Row>
+              <p>
+                I'm <span>Justice</span>, and I'm a <span>Full-Stack Web Engineer</span>.
+              </p>
+              <p>
+                I make mobile and web apps using everything ranging from <span>JavaScript</span> (ES6+)
+                and the <span>MERN</span> stack to <span>Dart</span> and <span>Flutter</span>.
+              </p>
+            </Row>
+          </div>
+          <hr />
+          <div className="portfolio">
+            <Row>
+              <p>Here are some of the projects I've worked on:</p>
+            </Row>
+            <Row>
+              <Card style={{ width: '30%', backgroundColor: 'gray', color:'white' }}>
+                <Card.Body>
+                  <Card.Title><p>He'inon</p></Card.Title>
+                  <Card.Text>
+                    An Arapaho language learning app.
+                  </Card.Text>
+                  <Button variant="primary" className="text">More Info</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+          </div>
+          <div className="contact">
+            <Row id="contact">
+              <p>
+                For more info{" "}
+                <a href="mailto:realjusticedunn@gmail.com">
+                  <span>contact me</span>
+                </a>
+                {" "}or check me out in the links below!
+              </p>
+            </Row>
+          </div>
+          </Col>
+          <Col className="d-none d-xl-block">
+            <img src={require("../../images/me.jpg")} alt="me"/>
+          </Col>
+        </Row>
       </Container>
     );
   }
