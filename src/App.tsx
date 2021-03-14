4921debab0f26a00f9166984848a29c51e7aa92a
Justice Dunn <justicedunn@me.com>
2021-03-14 15:51:59 -0600

    Update nav and footer styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./scenes/homePage/Home";
 import Blog from "./scenes/blogPage/Blog";
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,12 +7,12 @@ export default class NavBar extends Component {
   render() {
     return (
       <div className="d-none d-sm-block">
-        <Nav className="ml-auto nav fixed-top">
+        <Nav className="nav fixed-top">
           <div className="center">
             <div>
               <NavItem>
                 <Nav.Link eventKey="1" as={Link} to="/">
-                  <h3 className="brand">Justice_Dunn</h3>
+                  <h1 className="brand">Justice_Dunn</h1>
                 </Nav.Link>
               </NavItem>
             </div>
